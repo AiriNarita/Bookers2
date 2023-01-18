6b6fd9d88ec4dff8aba55db28e0f3c509eeee6c2
AiriNarita <119647023+AiriNarita@users.noreply.github.com>
2023-01-18 14:26:32 +0000

    [update]routes.rb / ルートパス,resouces

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/update'
-  get 'books/new'
-  get 'books/create'
-  get 'books/show'
-  get 'books/edit'
-  get 'books/update'
-  get 'books/destroy'
-  get 'homes/top'
+
   devise_for :users
+  root to: 'homes#top'
+
+  resources:users
+  resources:books
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+
 end
