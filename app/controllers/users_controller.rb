ae87e5e5090514f7b5e1f4e4f9ddd4779c3a7cd2
AiriNarita <119647023+AiriNarita@users.noreply.github.com>
2023-01-20 15:32:29 +0000

    [add]homes/top

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,8 @@ class UsersController < ApplicationController
   end
 
   def show
+    # @user = current_user
     @user = User.find(params[:id])
-
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
 
-  resources:users,:only => [:index, :show, :edit, :update,]
+  resources:users, :only => [:index, :show, :edit, :update,]
 
   get 'users/about', to: 'users#about'
   resources:books, :only => [:index, :new, :create,:show, :edit, :update, :destroy]
