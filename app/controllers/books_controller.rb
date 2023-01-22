37ae49ea61f546cf9b236df2086a3803a9923fb3
AiriNarita <119647023+AiriNarita@users.noreply.github.com>
2023-01-22 08:50:31 +0000

    [add]権限設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,8 @@
 class BooksController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
+
+
+
   def new
     @book = Book.new
   end
@@ -43,4 +47,12 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit( :title, :body)
   end
+
+
+  def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to(books_path) unless @user == current_user
+  end
+
 end
