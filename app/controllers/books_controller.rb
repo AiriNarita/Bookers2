44c06c06b13634fa20ff6f9741506d337ee21682
AiriNarita <119647023+AiriNarita@users.noreply.github.com>
2023-01-22 01:54:47 +0000

    [fix]book/edit controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,7 +18,7 @@ class BooksController < ApplicationController
    def index
     @book = Book.new
     @books = Book.all
-   
+
    end
 
   def edit
@@ -27,7 +27,7 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    Book.update(book_params)
+    book.update(book_params)
     redirect_to book_path(book.id)
   end
 
